eb87ff152044fc8bbbbd141a4417a1c944df75ed
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-03-02 16:03:46 +0100

    added turret and fisfhed orbit camera

--- Planet of Tonk/Assets/Script/mouse_aim.cs
+++ Planet of Tonk/Assets/Script/mouse_aim.cs
@@ -5,65 +5,42 @@ using UnityEngine;
 public class mouse_aim : MonoBehaviour
 {
     [SerializeField]
-	private GameObject FP;
-    
-    [SerializeField]
-	private GameObject TP;
-
-    [SerializeField]
-    private Transform player;
-
-    [SerializeField]
     private float sensitivity = 100f;
 
     private float xRotation = 0f;
     private float yRotation = 0f;
 
-    bool fstPerson = true;
-    // Start is called before the first frame update
+    //Orbit Camera
+    [SerializeField]
+    private Transform tformCamera;
+    [SerializeField]
+    private Transform tformParent;
+    [SerializeField]
+    private float orbSensitivity = 100f;
+
+    private Vector3 localRotation;
+
+    [SerializeField]
+    private float orbitDistance = 10f;
+
+    //Tell if I am in FPS or Orbit
+    bool fstPerson = false;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false; 
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void LateUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        localRotation.x += Input.GetAxis("Mouse X") * orbSensitivity;
+        localRotation.y -= Input.GetAxis("Mouse Y") * orbSensitivity;
+        localRotation.y = Mathf.Clamp(localRotation.y, 0f, 90f);
 
-        if(Input.GetKeyDown("v") == true)
-        {
-            if(fstPerson == false)
-            {
-                fstPerson = true;
-                FP.SetActive(true);
-                TP.SetActive(false);
-            }
-            else
-            {
-                fstPerson = false;
-                FP.SetActive(false);
-                TP.SetActive(true);
-            }
-        }
-
-        if(fstPerson == true)
-        {
-            xRotation -= mouseY;
-            yRotation += mouseX;
-            xRotation = Mathf.Clamp(xRotation, -80f, 80f);
-
-            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        }
-        else
-        {
-            xRotation -= mouseY;
-            yRotation += mouseX;
-            xRotation = Mathf.Clamp(xRotation, -80f, 80f);  
-
-            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        }
+        Quaternion QT = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+        tformParent.rotation = Quaternion.Lerp(tformParent.rotation, QT, Time.deltaTime * 3f);
+        tformCamera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(tformCamera.localPosition.z, orbitDistance * -1f, Time.deltaTime));
     }
 }
---
+++ Planet of Tonk/Assets/Script/mouse_aim_Prototype.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_aim_Prototype : MonoBehaviour
+{
+    //Tell if I am in FPS or Orbit
+    [SerializeField]
+    bool fstPerson = true;
+
+    //Comander Camera
+    [SerializeField]
+	private GameObject Camera;
+
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float sensitivity = 100f;
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    //Orbit Camera
+    [SerializeField]
+    private Transform tformCamera;
+    [SerializeField]
+    private Transform tformParent;
+    [SerializeField]
+    private float orbSensitivity = 100f;
+
+    private Vector3 localRotation;
+
+    [SerializeField]
+    private float orbitDistance = 10f;
+
+    private Quaternion lastRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        if(Input.GetKeyDown("v") == true)
+        {
+            lastRotation = Quaternion.Euler(tformParent.transform.localEulerAngles.x, tformParent.transform.localEulerAngles.y, 0);
+            if (fstPerson == false)
+            {
+                fstPerson = true;
+                tformCamera.transform.localPosition = new Vector3(0, 0, 0);
+                tformCamera.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                tformParent.transform.localRotation = lastRotation;
+            }
+            else
+            {
+                fstPerson = false;
+                tformCamera.transform.localPosition = new Vector3(0, 0, 0);
+                tformCamera.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                tformParent.transform.localRotation = lastRotation;
+            }
+        }
+
+        if(fstPerson == true)
+        {
+            xRotation -= mouseY;
+            yRotation += mouseX;
+            xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+            tformParent.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+    }
+    private void LateUpdate()
+    {
+        if (fstPerson == false)
+        {
+            localRotation.x += Input.GetAxis("Mouse X") * orbSensitivity;
+            localRotation.y -= Input.GetAxis("Mouse Y") * orbSensitivity;
+            localRotation.y = Mathf.Clamp(localRotation.y, -30f, 70f);
+
+            Quaternion QT = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+            tformParent.rotation = Quaternion.Lerp(tformParent.rotation, QT, Time.deltaTime*5f);
+            tformCamera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(tformCamera.localPosition.z, orbitDistance * -1f, Time.deltaTime));
+        }
+    }
+}
